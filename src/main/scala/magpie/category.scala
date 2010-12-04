4ebc71659caa9e659e0b3798ab94ba2fbe5f167f
Edward Kmett <ekmett@gmail.com>
2010-12-04 02:10:22 -0500

    product categories

--- src/main/scala/magpie/category.scala
+++ src/main/scala/magpie/category.scala
@@ -3,10 +3,12 @@ package magpie
 trait category[h <: hom.set] extends precategory[h] {
   def id[A>:h#inf<:h#sup]: h#hom[A,A]
   override def dual : category[hom.set.dual[h]] = category.op(this)
+  def *[g <: hom.set](that: category[g]) : category.product[h,g] = category.product[h,g](this,that)
 }
   
 object category { 
-  import equality._
+  import equality.{ === } 
+  import subtype.{ <~< }
   def duality[h<:hom.set] : category[h] === category[hom.set.dual[hom.set.dual[h]]] = 
      hom.set.duality[h].lift[Nothing,Any,({type λ[x<:hom.set] = category[x]})#λ]
 
@@ -16,7 +18,30 @@ object category {
 
   object op { 
     def apply[h<:hom.set](c : category[h]) : op[h] = new op[h] {
-      override def dual : category[hom.set.dual[hom.set.dual[h]]] = witness(duality[h])(c)
+      override def dual : category[hom.set.dual[hom.set.dual[h]]] = duality[h](c)
     }
   }
+
+  trait product[m<:hom.set,n<:hom.set] extends precategory.product[m,n] 
+   with category[hom.set.product[m,n]] 
+   with phantom.product[category[m], category[n]] {
+    def _1: category[m]
+    def _2: category[n]
+    // type h = hom.set.product[m,n] -- defined in precategory.product
+    def id[A>:h#inf<:h#sup]: h#hom[A,A] = hom.product[m,n,A,A](_1.id[A#_1],_2.id[A#_2])
+    // TODO: override def dual : product[hom.set.dual[m],hom.set.dual[n]]
+    override def dual =
+        product.duality[m,n](product[hom.set.dual[m],hom.set.dual[n]](_1.dual,_2.dual))
+  } 
+  object product {
+    def apply[m<:hom.set,n<:hom.set](m: category[m], n: category[n]): product[m,n] = new product[m,n] {
+      def _1: category[m] = m
+      def _2: category[n] = n
+    }
+    def duality[m<:hom.set,n<:hom.set] 
+    : category[hom.set.product[hom.set.dual[m],hom.set.dual[n]]] <~<
+      category[hom.set.dual[hom.set.product[m,n]]] = 
+      hom.set.product.duality[m,n].lift[Nothing,Any,({type λ[+x<:hom.set] = category[x]})#λ]
+  }
 }
+
--- src/main/scala/magpie/hom.scala
+++ src/main/scala/magpie/hom.scala
@@ -3,8 +3,8 @@ package magpie
 /** currently these play the roles of both hom-classes and hom-sets */
 object hom { 
   import equality._
-  /** hom-sets are modeled as a range of types */
-  trait set { self => 
+  /** hom-sets are modeled as a range of types. sealed to ensure the validity of the duality laws */
+  sealed trait set { self => 
     type inf
     type sup >: inf
     type hom[_>:inf<:sup,_>:inf<:sup] 
@@ -16,27 +16,35 @@ object hom {
       type sup = h#sup
       type hom[a>:inf<:sup,b>:inf<:sup] = h#hom[b,a]
     }
-    /** hom.set.of makes it more convenient to construct a particular hom.set */
+    /** hom.set.of: convenient accessor to construct a particular hom.set */
     type of[l,h>:l,c[_>:l<:h,_>:l<:h]] = set {
       type inf = l
       type sup = h
       type hom[x>:l<:h,y>:l<:h] = c[x,y]
     }
 
-    /** creates a hom-set that consists of a single type */
+    /** hom.set.singleton: creates a hom-set that consists of a single type */
     type singleton[z] = of[z,z,({type λ[x>:z<:z,y>:z<:z] = z})#λ]
 
-    /** hack to witness the equality of a hom-set to its dual dual */
+    /** hom.set.duality: hack to witness the equality of a hom-set to its dual dual */
     def duality[h<:set] : equality[Nothing,set,h,dual[dual[h]]] = 
-      refl[h].asInstanceOf[equality[Nothing,set,h,dual[dual[h]]]]
+      equality.refl[h].asInstanceOf[equality[Nothing,set,h,dual[dual[h]]]]
 
+    /** hom.set.product: the product of two hom-sets */
     type product[x<:set,y<:set] = set { 
       type inf = phantom.product[x#inf,y#inf]
       type sup = phantom.product[x#sup,y#sup]
       type hom[a>:inf<:sup,b>:inf<:sup] = hom.product[x,y,a,b]
     }
+
+    object product { 
+      /** hom.set.product.duality: another bald-faced assertion about duality. */
+      def duality[x <: set, y <: set] : subtype[Nothing,set,product[dual[x],dual[y]],dual[product[x,y]]] = 
+        subtype.refl[product[dual[x],dual[y]]].asInstanceOf[subtype[Nothing,set, product[dual[x],dual[y]], dual[product[x,y]]]]
+    }
   }
 
+  /* hom.product provides a concrete implementation of the product of two hom-set morphisms */
   case class product[
     x<:hom.set,
     y<:hom.set,
