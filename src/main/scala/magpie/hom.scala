6dd3bcc0b4a0b80fd20d3561d6a3ab4932843f6c
Edward Kmett <ekmett@gmail.com>
2010-12-05 08:29:25 -0500

    experimental "dinatural" hom.sets -- breaks everything

--- src/main/scala/magpie/hom.scala
+++ src/main/scala/magpie/hom.scala
@@ -2,34 +2,43 @@ package magpie
 
 /** currently these play the roles of both hom-classes and hom-sets */
 object hom { 
-  import equality._
+//  import equality._
   /** hom-sets are modeled as a range of types. 
     * sealed to ensure the validity of the duality laws 
     * and that it is never actually constructed
     */
   sealed trait set { self => 
-    type inf
-    type sup >: inf
-    type hom[_>:inf<:sup,_>:inf<:sup] 
+    type obinf
+    type obsup >: obinf
+    type ob[-_>:obinf<:obsup,+_>:obinf<:obsup]
+    type inf = ob[obsup,obinf]
+    type sup = ob[obinf,obsup]
+    type hom[-_>:inf<:sup,+_>:inf<:sup] 
   }
 
   object set {
     /** the dual of a hom-set is a hom-set */
     type dual[h<:set] = set { 
+			type obinf = h#obinf
+      type obsup = h#obsup
+      type ob[-a>:obinf<:obsup,+b>:obinf<:obsup] = h#ob[b,a]
       type inf = h#inf
       type sup = h#sup
-      type hom[a>:inf<:sup,b>:inf<:sup] = h#hom[b,a]
+      type hom[-a>:inf<:sup,+b>:inf<:sup] = h#hom[b,a]
     }
 
     /** hom.set.of: convenient accessor to construct a particular hom.set */
-    type of[l,h>:l,c[_>:l<:h,_>:l<:h]] = set {
-      type inf = l
-      type sup = h
-      type hom[x>:l<:h,y>:l<:h] = c[x,y]
+    type of[l, h>:l, o[-_>:l<:h,+_>:l<:h], c[-_>:o[h,l]<:o[l,h],+_>:o[h,l]<:o[l,h]]] = set {
+      type obinf = l
+      type obsup = h
+      type ob[-a>:obinf<:obsup,+b>:obinf<:obsup] = o[a,b]
+      type inf = ob[obsup,obinf]
+      type sup = ob[obinf,obsup]
+      type hom[-a>:inf<:sup,+b>:inf<:sup] = c[a,b]
     }
 
     /** hom.set.singleton: creates a hom-set that consists of a single type */
-    type singleton[z] = of[z,z,({type λ[x>:z<:z,y>:z<:z] = z})#λ]
+    type singleton[z] = of[z,z,({type λ[-x>:z<:z,+y>:z<:z]=z})#λ,({type λ[-x>:z<:z,+y>:z<:z] = z})#λ]
 
     /** hom.set.duality: hack to witness the equality of a hom-set to its dual dual */
     def duality[h<:set] : equality[Nothing,set,h,dual[dual[h]]] = 
@@ -37,9 +46,22 @@ object hom {
 
     /** hom.set.product: the product of two hom-sets */
     type product[x<:set,y<:set] = set { 
+      type obinf = typed.product[x#obinf,y#obinf]
+      type obsup = typed.product[x#obsup,y#obsup]
+      type ob[-a>:obinf<:obsup,+b>:obinf<:obsup] = typed.product[x#ob [a#_1,b#_1], y#ob [a#_2,b#_2]]
+
+      // busted:
+      // type inf = ob[obsup,obinf]// -- compiler bug. see reasoning
+      // type sup = ob[obinf,obsup]
+
+      // the full expansion doesn't work at all:
+      // type inf = typed.product[x#ob[x#obsup,x#obinf], y#ob[y#obsup, y#obinf]]
+      // type sup = typed.product[x#ob[x#obinf,x#obsup], y#ob[y#obinf, y#obsup]]
+
+      // works until instantiation:
       type inf = typed.product[x#inf,y#inf]
       type sup = typed.product[x#sup,y#sup]
-      type hom[a>:inf<:sup,b>:inf<:sup] = typed.product[x#hom[a#_1,b#_1], y#hom[a#_2,b#_2]]
+      type hom[-a>:inf<:sup,+b>:inf<:sup] = typed.product[x#hom[a#_1,b#_1], y#hom[a#_2,b#_2]]
     }
     object product { 
       def hom[
--- src/main/scala/magpie/precategory.scala
+++ src/main/scala/magpie/precategory.scala
@@ -9,8 +9,8 @@ package magpie
  */ 
 trait precategory[h <: hom.set] {
   def compose[A>:h#inf<:h#sup, B>:h#inf<:h#sup, C>:h#inf<:h#sup](f : h#hom[B,C], g: h#hom[A,B]) : h#hom[A,C]
-  def dual : precategory[hom.set.dual[h]] = precategory.op(this)
-  def *[g <: hom.set](that: precategory[g]) : precategory.product[h,g] = precategory.product[h,g](this,that)
+  def dual : precategory[hom.set.dual[h]] // = precategory.op(this)
+  // def *[g <: hom.set](that: precategory[g]) : precategory.product[h,g] = precategory.product[h,g](this,that)
 }
   
 object precategory { 
@@ -19,8 +19,9 @@ object precategory {
   def duality[h<:hom.set] : precategory[h] === precategory[hom.set.dual[hom.set.dual[h]]] = 
      hom.set.duality[h].lift[Nothing,Any,({type λ[x<:hom.set] = precategory[x]})#λ]
 
+/*
   trait op[h <: hom.set] extends precategory[hom.set.dual[h]] { 
-    def compose[A>:h#inf<:h#sup, B>:h#inf<:h#sup, C>:h#inf<:h#sup](f : h#hom[C,B], g: h#hom[B,A]) : h#hom[C,A] = dual.compose[C,B,A](g, f)
+    // def compose[A>:hom.set.dual[h]#inf<:hom.set.dual[h]#sup, B>:hom.set.dual[h]#inf<:hom.set.dual[h]#sup, C>:hom.set.dual[h]#inf<:hom.set.dual[h]#sup](f : hom.set.dual[h]#hom[C,B], g: hom.set.dual[h]#hom[A,B]) : hom.set.dual[h]#hom[A,C] = dual.compose[A,B,C](g, f)
   }
   object op { 
     def apply[h<:hom.set](c : precategory[h]) : op[h] = new op[h] {
@@ -36,7 +37,6 @@ object precategory {
     def _2: precategory[n]
     type h = hom.set.product[m,n]
     def compose[A>:h#inf<:h#sup, B>:h#inf<:h#sup, C>:h#inf<:h#sup](f : h#hom[B,C], g: h#hom[A,B]) : h#hom[A,C] = 
-     // hom.product[m,n,A,C]
      typed.product(
        _1.compose[A#_1,B#_1,C#_1](f._1,g._1),
        _2.compose[A#_2,B#_2,C#_2](f._2,g._2)
@@ -55,4 +55,5 @@ object precategory {
       precategory[hom.set.dual[hom.set.product[m,n]]] = 
       hom.set.product.duality[m,n].colift[Nothing,Any,({type λ[+x<:hom.set] = precategory[x]})#λ]
   }
+*/
 }
